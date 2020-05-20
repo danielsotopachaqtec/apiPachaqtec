ec17390cd5f11e0fbc6dc593f5004138bf8f99a3
Daniel Alberto Soto Guillen <daniel.alberto.soto@LIM-WS1814.local>
2020-05-19 21:08:53 -0500

    [feature] - install nodemon & morgan & handling errors

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// Handle incoming GET requests to /products
 router.get('/', (req,res,next) => {
     res.status(200).json({
         message: 'Handling GET request to /products'
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const port = 3000;
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'));
 // app.get('/', function (req, res) {
 //     res.status(200).json({
 //         message: 'it works'
@@ -13,6 +15,19 @@ const orderRoutes = require('./api/routes/orders')
 //   });
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use((req,res,next)=> {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+})
+app.use((error, req,res, next)=> {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 });
