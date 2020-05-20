c1625a297b2c7a69c7b871d78f498542f0243904
Daniel Alberto Soto Guillen <daniel.alberto.soto@LIM-WS1814.local>
2020-05-19 20:55:09 -0500

    [feature] - add orders & products routes

---
+++ api/routes/orders.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res,next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST request to /orders'
+    })
+})
+
+router.get('/:orderId', (req,res,next) => {
+    const id = req.params.orderId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the special order ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed and order ID'
+        })
+    }
+})
+
+router.patch('/:orderId', (req,res,next)=> {
+    res.status(200).json({
+        message: 'Updated order'
+    })
+})
+
+router.put('/:orderId', (req,res,next)=> {
+    res.status(200).json({
+        message: 'Updated order'
+    })
+})
+
+router.delete('/:orderId', (req,res,next)=> {
+    res.status(200).json({
+        message: 'Delete order'
+    })
+})
+
+module.exports = router;
--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', function (req, res) {
-    res.status(200).json({
-        message: 'it works'
-    });
-  });
+const productRoutes = require('./api/routes/products')
+const orderRoutes = require('./api/routes/orders')
+
+// app.get('/', function (req, res) {
+//     res.status(200).json({
+//         message: 'it works'
+//     });
+//   });
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 });
