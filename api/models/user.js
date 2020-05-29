2f8c230db61947991046182c1737be457b82d0f9
Daniel Alberto Soto Guillen <daniel.alberto.soto@LIM-WS1814.local>
2020-05-28 21:55:51 -0500

    [feature] - add utils validator & setup jwt sign & login

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const Utils = require('../../utils/resource')
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true },
-    password: String,
-    phoneNumber: { type: String, required: true}
+    email: { type: String, required: true, match: Utils.validator.formatEmail },
+    password: { type: String, required: true},
+    phoneNumber: { type: String, required: true, unique: true}
 });
 
 module.exports = mongoose.model('User', userSchema);
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,46 +2,137 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
-router.post('/signup', (req,res, next) => {
-    bcrypt.genSalt(10, (err, salt) =>{
-        bcrypt.hash(req.body.password, salt, (err, hash)=> {
-            // Store hash in your password DB.
-            if(err){
-                console.log('err',err)
-                return res.status(500).json({
-                    errors: true,
-                    message: err
-                })
-            } else {
-                console.log('password', hash)
-                const user = new User({
-                    _id: new mongoose.Types.ObjectId(),
-                    email: req.body.email,
-                    password: hash,
-                    phoneNumber: req.body.phoneNumber
-                });
-                user
-                .save()
-                .then((result) => {
-                    console.log('result signup', result)
-                    res.status(201).json({
-                        message: "Handling POST request to /users",
-                        data: result
-                    })
-                })
-                .catch(err=> {
-                    console.log('err', err);
-                    res.status(500).json({
+router.post('/signin', (req,res,next) => {
+    User.find({  phoneNumber: req.body.phoneNumber})
+    .exec()
+    .then((user) => {
+        if(user.length < 1){
+            return res.status(401).json({
+                errors: true,
+                message: 'Phone number not found!, user doesn\t exist'
+            })
+        } else {
+            bcrypt.compare(req.body.password, user[0].password, (err,result) => {
+                if(err){
+                    return res.status(401).json({
                         errors: true,
-                        message: err
+                        message: 'Auth failed!'
                     })
+                }
+                if(result){
+                    const token = jwt.sign(
+                        {
+                            phoneNumber: user[0].phoneNumber,
+                            userId: user[0]._id
+                        }, process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                    )
+                    return res.status(200).json({
+                        message: "Auth successful",
+                        token: token
+                    })
+                }
+                return res.status(401).json({
+                    errors: true,
+                    message: 'Auth failed!'
                 })
-            }
-        });
-    });
+            })
+        }
+    })
+    .catch(err=> {
+        console.log('err', err);
+        res.status(500).json({
+            errors: true,
+            message: err
+        })
+    })
+})
+
+router.post('/signup', (req,res, next) => {
+    User.find({  phoneNumber: req.body.phoneNumber})
+    .exec()
+    .then((user) => {
+        if(user.length >= 1){
+            return res.status(409).json({
+                errors: true,
+                message: 'PhoneNumber exists'
+            })
+        } else {
+            bcrypt.genSalt(10, (err, salt) =>{
+                bcrypt.hash(req.body.password, salt, (err, hash)=> {
+                    // Store hash in your password DB.
+                    if(err){
+                        console.log('err',err)
+                        return res.status(500).json({
+                            errors: true,
+                            message: err
+                        })
+                    } else {
+                        console.log('password', hash)
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash,
+                            phoneNumber: req.body.phoneNumber
+                        });
+                        user
+                        .save()
+                        .then((result) => {
+                            console.log('result signup', result)
+                            res.status(201).json({
+                                message: "Handling POST request to /users",
+                                data: result,
+                                request: {
+                                    type: 'DELETE',
+                                    url: 'http://localhost:3000/users/' + result.id 
+                                }
+                            })
+                        })
+                        .catch(err=> {
+                            console.log('err', err);
+                            res.status(500).json({
+                                errors: true,
+                                message: err
+                            })
+                        })
+                    }
+                });
+            });
+        }
+    })
+    .catch(err=> {
+        console.log('err', err);
+        res.status(500).json({
+            errors: true,
+            message: err
+        })
+    })
 });
+
+router.delete('/:userId', (req,res,next)=> {
+    const id = req.params.userId;
+    User.remove({_id: id})
+    .exec()
+    .then(result=> {
+        console.log('result', result)
+        res.status(200).json({
+            message: 'Handling Delete request to /userId',
+            data: result
+        })
+    })
+    .catch(err=> {
+        console.log('err', err);
+        res.status(500).json({
+            errors: true,
+            message: err
+        })
+    })
+})
 
 module.exports = router;
